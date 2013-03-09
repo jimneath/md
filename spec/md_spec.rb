f93458c2e9398ea38f9986f27cdefcad7c5721c7
Jim Neath <jimneath@googlemail.com>
2013-03-09 20:29:42 +0000

    added more skeleton stuff

---
+++ spec/md_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Md do
+  it 'should have a version number' do
+    Md::VERSION.should_not be_nil
+  end
+
+  it 'should do something useful' do
+    false.should be_true
+  end
+end
